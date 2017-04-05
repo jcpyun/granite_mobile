2cdbb94a2b73d945c87ffce787fc2307f0623283
jcpyun <johnchpyun@gmail.com>
2017-04-04 22:58:36 -0400

    wow

---
+++ granite_mobile_v1.2/www/js/opinfo.js
@@ -0,0 +1,174 @@
+var opinfo={
+  "agotsis":{
+    "Name": "Alexander Gotsis",
+    "Committee": "Head of Electrical"
+  },
+  "awhyte":{
+    "Name": "Allison Whyte",
+    "Committee": "Operations"
+  },
+  "amartine":{
+    "Name": "Amalia Martinez",
+    "Committee": "Chair"
+  },
+  "asgreen":{
+    "Name": "Andrew Greenwald",
+    "Committee": "Advisor"
+  },
+  "ayang1":{
+    "Name": "Annie Yang",
+    "Committee": "Operations"
+  },
+  "zhenghaj":{
+    "Name": "Brandon Jin",
+    "Committee": "Operations"
+  },
+  "cmorin":{
+    "Name": "Caroline Morin",
+    "Committee": "Operations"
+  },
+  "clairec1":{
+    "Name": "Claire Chen",
+    "Committee": "Entertainment"
+  },
+  "dkaushal":{
+    "Name": "Dhruva Kaushal",
+    "Committee": "Entertainment"
+  },
+  "jlareau":{
+    "Name": "Dillon Lareau",
+    "Committee": "Electrical"
+  },
+  "egarbade":{
+    "Name": "Edward Garbade",
+    "Committee": "Operations"
+  },
+  "epnewman":{
+    "Name": "Emily Newman",
+    "Committee": "Operations"
+  },
+  "grickstr":{
+    "Name": "Gabrielle Rickstrew",
+    "Committee": "Head of Marketing"
+  },
+  "hkoschme":{
+    "Name": "Hannah Koschmeder",
+    "Committee": "Head of Entertainment"
+  },
+   "jacobbro":{
+    "Name": "Jacob Brooks",
+    "Committee": "Booth"
+  },
+  "jwesson":{
+    "Name": "Jacob Wesson",
+    "Committee": "Head of Electrical"
+  },
+  "jlliang":{
+    "Name": "Jarrett Liang",
+    "Committee": "Operations"
+  },
+  "jcmertz":{
+    "Name": "Joe Mertz",
+    "Committee": "Head of Operations"
+  },
+  "jbz":{
+    "Name": "Jeremy Zhang",
+    "Committee": "Operations"
+  },
+  "jcp":{
+    "Name": "John Pyun",
+    "Committee": "Electrical"
+  },
+  "jzak":{
+    "Name": "Josh Zak",
+    "Committee": "Head of Booth"
+  },
+  "jsatvoll":{
+    "Name": "Julien Sat-Vollhardt",
+    "Committee": "Operations"
+  },
+  "kmg":{
+    "Name": "Kara Gadecki",
+    "Committee": "Booth"
+  },
+   "kborst":{
+    "Name": "Kate Borst",
+    "Committee": "Booth"
+  },
+  "kmquinn":{
+    "Name": "Kayla Quinn",
+    "Committee": "Operations"
+  },
+ "lbressle":{
+    "Name": "Laura Bressler",
+    "Committee": "Electrical"
+  },
+  "lgmiller":{
+    "Name": "Lauren Miller",
+    "Committee": "Operations"
+  },
+ "lawrencx":{
+    "Name": "Lawrence Xu",
+    "Committee": "Entertainment"
+  },
+  "mreager":{
+    "Name": "Margaret Reager",
+    "Committee": "Asst. Head of Ops"
+  },
+  "madedjou":{
+    "Name": "Maskana Adedjouman",
+    "Committee": "Operations"
+  },
+  "meribyte":{
+    "Name": "Meg Richards",
+    "Committee": "Golfcarts"
+  },
+  "mjvultag":{
+    "Name": "Mike Vultaggio",
+    "Committee": "Electrical"
+  },
+  "ncoauett":{
+    "Name": "Nicholas Coauette",
+    "Committee": "Head of Booth Operations"
+  },
+  "nehull":{
+    "Name": "Noah Hull",
+    "Committee": "Operations"
+  },
+  "prheinhe":{
+    "Name": "Philip Rheinheimer",
+    "Committee": "Electrical"
+  },
+  "ryerzhan":{
+    "Name": "Renata Yerzhanova",
+    "Committee": "Operations"
+  },
+  "swu1":{
+    "Name": "Samantha Wu",
+    "Committee": "Operations"
+  },
+  "stacycha":{
+    "Name": "Stacy Chang",
+    "Committee": "Entertainment"
+  },
+  "saclark":{
+    "Name": "Stephen Clark",
+    "Committee": "Asst. Head of Ops"
+  },
+  "tford":{
+    "Name": "Thomas Ford",
+    "Committee": "Marketing"
+  },
+  "wlowe":{
+    "Name": "William Lowe",
+    "Committee": "Operations"
+  },
+ "zsolan":{
+    "Name": "Zhuri Solan",
+    "Committee": "Entertainment"
+  },
+"zileig":{
+    "Name": "Zilei Gu",
+    "Committee": "Treasurer"
+  }
+}
